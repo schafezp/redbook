0fecbf3d9b2ca6e8d51043dda2eccd36815b09ad
Zachary Schafer <schafezp@gmail.com>
2019-01-06 22:57:01 -0500

    move handler to the top

--- handlers.go
+++ handlers.go
@@ -9,6 +9,32 @@ import (
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api"
 )
 
+//Handler main point of entry for handlers
+func Handler(bot tgbotapi.BotAPI, update tgbotapi.Update, db *db.DB) {
+
+	print("Handling bot api")
+	log.Printf("Is user making prediction %t", isUserMakingPrediction(update.Message.From.ID, db))
+
+	if update.Message.IsCommand() {
+		msg := tgbotapi.NewMessage(update.Message.Chat.ID, "")
+		switch update.Message.Command() {
+		case "start":
+			msg.Text = "Starting message"
+			bot.Send(msg)
+		case "predict":
+			startPrediction(bot, update, db)
+		default:
+			msg.Text = "Unknown command"
+			bot.Send(msg)
+		}
+	} else {
+		if isUserMakingPrediction(update.Message.From.ID, db) {
+			submitPrediction(*update.Message, db, bot)
+		}
+	}
+
+}
+
 //TODO: add command to undo
 
 //passed in
@@ -40,31 +66,5 @@ func submitPrediction(message tgbotapi.Message, db *db.DB, bot tgbotapi.BotAPI) 
 	db.Query("DELETE FROM prediction_in_progress WHERE userid=$1", message.From.ID)
 	msg := tgbotapi.NewMessage(message.Chat.ID, "Submitted prediction")
 	bot.Send(msg)
-
-}
-
-//Handler main point of entry for handlers
-func Handler(bot tgbotapi.BotAPI, update tgbotapi.Update, db *db.DB) {
-
-	print("Handling bot api")
-	log.Printf("Is user making prediction %t", isUserMakingPrediction(update.Message.From.ID, db))
-
-	if update.Message.IsCommand() {
-		msg := tgbotapi.NewMessage(update.Message.Chat.ID, "")
-		switch update.Message.Command() {
-		case "start":
-			msg.Text = "Starting message"
-			bot.Send(msg)
-		case "predict":
-			startPrediction(bot, update, db)
-		default:
-			msg.Text = "Unknown command"
-			bot.Send(msg)
-		}
-	} else {
-		if isUserMakingPrediction(update.Message.From.ID, db) {
-			submitPrediction(*update.Message, db, bot)
-		}
-	}
 
 }

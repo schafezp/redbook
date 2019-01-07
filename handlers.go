f437399c80e0d56d45ffee632693008c3de121ed
Zachary Schafer <schafezp@gmail.com>
2019-01-06 22:55:02 -0500

    check if user is making prediction

--- handlers.go
+++ handlers.go
@@ -3,6 +3,7 @@ package redbook
 import (
 	"database/sql"
 	db "database/sql"
+	"fmt"
 	"log"
 
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api"
@@ -19,16 +20,24 @@ func startPrediction(bot tgbotapi.BotAPI, update tgbotapi.Update, db *db.DB) {
 }
 
 func isUserMakingPrediction(userID int, db *db.DB) bool {
-	pred := CreatingPrediction{}
-	row, _ := db.Query("SELECT userid FROM prediction_in_progress WHERE userid = $1", userID)
-	err := row.Scan(&pred.UserId)
-	//if err there are no rows, user not making prediction
-	return err != sql.ErrNoRows
+	rows := db.QueryRow("SELECT EXISTS(SELECT userid FROM prediction_in_progress WHERE userid = $1)", userID)
+	var isUserMakingPred bool
+	switch err := rows.Scan(&isUserMakingPred); err {
+	case sql.ErrNoRows:
+		fmt.Println("No rows were returned!")
+		return true
+	case nil:
+		fmt.Println("User pred ", isUserMakingPred)
+		return isUserMakingPred
+	default:
+		panic(err)
+	}
+
 }
 
 func submitPrediction(message tgbotapi.Message, db *db.DB, bot tgbotapi.BotAPI) {
 
-	db.Query("DELETE prediction_in_progress WHERE userid=$1", message.From.ID)
+	db.Query("DELETE FROM prediction_in_progress WHERE userid=$1", message.From.ID)
 	msg := tgbotapi.NewMessage(message.Chat.ID, "Submitted prediction")
 	bot.Send(msg)
 
@@ -48,13 +57,6 @@ func Handler(bot tgbotapi.BotAPI, update tgbotapi.Update, db *db.DB) {
 			bot.Send(msg)
 		case "predict":
 			startPrediction(bot, update, db)
-
-			/* for rows.Next() {
-				prediction := redbook.CreatingPrediction{}
-				rows.Scan(&prediction.UserId)
-				println("userid: " + strconv.Itoa(prediction.UserId))
-			} */
-
 		default:
 			msg.Text = "Unknown command"
 			bot.Send(msg)

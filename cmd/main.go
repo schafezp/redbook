4ecaffa9580f39a25c96ef7ebcfcefbd82fa8840
Zachary Schafer <schafezp@gmail.com>
2019-01-06 23:12:27 -0500

    remove spam log

--- cmd/main.go
+++ cmd/main.go
@@ -35,14 +35,14 @@ func main() {
 	u := tgbotapi.NewUpdate(0)
 	u.Timeout = 60
 
-	updates, err := bot.GetUpdatesChan(u)
+	updates, _ := bot.GetUpdatesChan(u)
 
 	for update := range updates {
 		if update.Message == nil { // ignore any non-Message Updates
 			continue
 		}
 
-		log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
+		//log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
 
 		redbook.Handler(*bot, update, db)
 	}
